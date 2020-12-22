1a0dfbac6dea5cfcbbed82e955c9439cec5729fe
JuanequeX <ramon.g.uz37@gmail.com>
2020-12-21 20:55:05 -0600

    Create navigation bar
    In this commit creates a dynamic navigation bar component with
    an event window scroll

---
+++ app/javascript/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header';
+
+const Header = () => {
+  return (
+    <header className='header-content'>
+      <h4 className='header-content__title'>be the title</h4>
+    </header>
+  );
+};
+
+export default Header;
